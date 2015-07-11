15b7a782209e647d98931272b958833b38ea9224
flueg <fluegliu@gmail.com>
2015-07-11 17:13:45 +0800

    Add comment for my code.

--- eightQueen/main.cpp
+++ eightQueen/main.cpp
@@ -12,6 +12,14 @@ int queen[QUEEN_NUMBER + 1];
 int totalSolution = 1;
 std::ofstream output;
 
+//typedef struct Queen
+//{
+//	int number;
+//	int diagonalX;
+//	int disgonalY;
+//} Queen;
+//Queen queen[QUEEN_NUMBER + 1];
+
 int compare(const void* i, const void* j)
 {
 	if (*(int*)i < *(int*)j) return -1;
@@ -19,9 +27,11 @@ int compare(const void* i, const void* j)
 	else return 1;
 }
 
+// Successfully get a solution.
+// Output the solution.
 void succeed()
 {
-	std::cout << "Solution #" << totalSolution++ << ":" << std::endl;
+	std::cout << "Solution #" << totalSolution << ":" << std::endl;
 	output << "Solution #" << totalSolution++ << ":" << std::endl;
 	for (int i = 1; i <= QUEEN_NUMBER; i++)
 	{
@@ -32,13 +42,14 @@ void succeed()
 	output << std::endl;
 }
 
+// Test if we place Queen n in column queen[n] is approperiate.
 bool sanityTest(int n)
 {
 	int diagonalX[n+1], diagonalY[n+1];
 	for (int i = 1; i <= n; i++)
 	{
 		diagonalX[i] = queen[i] - i;
-		diagonalY[i] = i + queen[i] - 8;
+		diagonalY[i] = i + queen[i] - QUEEN_NUMBER;
 	}
 
 	qsort(&diagonalX[1], n, sizeof(int), &compare);
@@ -47,42 +58,34 @@ bool sanityTest(int n)
 	for (int i = 1; i < n; i++)
 		if (diagonalX[i] == diagonalX[i + 1] || diagonalY[i] == diagonalY[i + 1])
 			return false;
-
-	//for (int i = 1; i <= n; i++)
-	//	for (int j = 1; j < i; j++)
-	//		if ((queen[i] - i) == (queen[j] - j) || (j - 8 + queen[j]) == (i - 8 + queen[i]))
-	//			return false;
 	return true; 
 }
 
+/*
+ * Function to recurringly pernutate queen solutions.
+ */
 void Permutation(int n)
 {
-//for (int i = 1; i < n; i++)
-//{
-//	std::cout << queen[i] << " ";
-//}
-//std::cout << std::endl;
-
 	if (n > QUEEN_NUMBER)
 	{
 		succeed();
 		return;
 	}
 
-//std::cout << "n is: " << n << std::endl;
 	for (int i = 1; i <= QUEEN_NUMBER; i++)
 	{	
+		// Column i might already has a queen.
 		if (isValid[i])
-		{ 
+		{
+			// Try to set Quenn n in column i.
 			queen[n] = i;
-//std::cout << "Process " << i << std::endl;
+			// Test if Queen n could be set in column i.
 			if (sanityTest(n))
 			{
-				isValid[i] = false; // Since i is used, mark it as ! invalid.
+				isValid[i] = false; // Since column i is used, mark it as ! invalid.
 				Permutation(n + 1);
 				isValid[i] = true; // Fall back
 			}
-			//else std::cout<< " failed " << n;
 		}
 	}
 }
