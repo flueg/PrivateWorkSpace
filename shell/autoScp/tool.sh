ba0835c75cb4d3516b6c09a8084d06544e803c2a
flueg <fluegliu@gmail.com>
2016-04-08 15:39:09 +0800

    auto scp shell expect script

---
+++ shell/autoScp/tool.sh
@@ -0,0 +1,28 @@
+
+kkscp()
+{
+	send_file=${1}
+	host=${2:-"essh.sandai.net:/home/usrname/packages/"}	
+	usrname=${3:-"usrname"}
+	passwd=${4:-passwd}
+
+	if [ "x$send_file" = "x" ]; then
+		echo "Usage: kkscp <sendfile> [<host> <usrname> <passwd>]"
+		return 1
+	elif [ ! -f $send_file ]; then
+		echo "$send_file is not found"
+		return 2
+	fi
+
+	SSH=scp.expect
+	cat <<-EXPECT_SCRIPTS > $SSH
+	#!/usr/bin/expect
+	spawn /usr/bin/scp $send_file $usrname@$host
+	expect "$usrname@essh.sandai.net*"
+	send "$passwd\r"
+	expect "$send_file*"
+	EXPECT_SCRIPTS
+	chmod 755 $SSH
+	./$SSH
+	rm -f $SSH
+}
