9fbd93dd141599aeb36cea07afa6a082c67228a2
flueg <fluegliu@gmail.com>
2016-06-03 00:24:34 +0800

    gofmt codes.

--- go/map_forrange.go
+++ go/map_forrange.go
@@ -1,14 +1,16 @@
 package main
+
 import "fmt"
+
 func main() {
-     map1 := make(map[int]float32)
-        map1[3] = 3.0
-       map1[2] = 2.0
-      map1[1] = 1.0
-      //The Way to Go
-         map1[4] = 4.0
-         // Elements in map are not key-ordered.
-         for key, value := range map1 {
-             fmt.Printf("key is: %d - value is: %f\n", key, value)
-              }
+	map1 := make(map[int]float32)
+	map1[3] = 3.0
+	map1[2] = 2.0
+	map1[1] = 1.0
+	//The Way to Go
+	map1[4] = 4.0
+	// Elements in map are not key-ordered.
+	for key, value := range map1 {
+		fmt.Printf("key is: %d - value is: %f\n", key, value)
+	}
 }
--- go/string_extraction.go
+++ go/string_extraction.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-    s := "hello"
-    fmt.Println(s)
-    c := []byte(s)
-    c[0]='c'
-    s2:= string(c) // s2 == "cello"
-    fmt.Println(s2)
+	s := "hello"
+	fmt.Println(s)
+	c := []byte(s)
+	c[0] = 'c'
+	s2 := string(c) // s2 == "cello"
+	fmt.Println(s2)
 }
